ee692e09926c5bbd514345563326ef4eec0f9608
shelton gillum <sgillum21@gmail.com>
2023-04-26 21:08:37 -0700

    backend server added

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,8 +1,74 @@
-import React from "react"
-import ProfilePage from "./pages/authPages/ProfilePage"
-const App = ()=>{
+import React, {useContext} from "react"
+import { Routes, Route, Navigate } from 'react-router-dom'
+import {UserContext} from "./context/UserProvider"
+import LandingPage from "./pages/authPages/LandingPage"
+import ProfilePage from "./pages/profilePages/ProfilePage"
+import PublicRecipesPage from "./pages/publicRecipeFolder/PublicRecipesPage"
+import MyRecipesPage from "./pages/userRecipeFolder/MyRecipesPage"
+import CreateRecipePage from "./pages/userRecipeFolder/CreateRecipePage"
+
+export default function App(props){
+    const {token,
+          user,
+          deleteRecipe,
+          addRecipe,
+          logout,
+          editRecipe,
+          deleteAccount,
+          ...initChefState
+        } = useContext(UserContext)
     return(
-        <ProfilePage/>
+        <div>
+         {/* { token  } */}
+      <Routes>
+        <Route 
+          path="/" 
+          element={token ? <Navigate to="/profilePage"/> : <LandingPage />}
+        />
+         <Route 
+          path="/profilePage" 
+          element={
+            <ProtectedRoute token={token} redirectTo="/">
+              <ProfilePage 
+              deleteAccount={deleteAccount} 
+              username={user.username} 
+              id={user._id}
+              logout={logout}
+              />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/publicRecipesPage" 
+          element={
+            <ProtectedRoute token={token} redirectTo="/">
+              <PublicRecipesPage all={initChefState.allRecipes} currentUser={user}/>
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/myRecipesPage" 
+          element={
+            <ProtectedRoute token={token} redirectTo="/">
+              <MyRecipesPage 
+              recipes={initChefState.myRecipes}
+              chef={user}
+              deleteFood={deleteRecipe}
+              add={addRecipe}
+              edit={editRecipe}
+              />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/createRecipePage" 
+          element={
+            <ProtectedRoute token={token} redirectTo="/">
+              <CreateRecipePage submit={addRecipe}/>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </div>
     )
 }
-export default App
